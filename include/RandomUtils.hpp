f867b64bb514128921d5ece31e6a020db369e468
Pandicon <70060103+Pandicon@users.noreply.github.com>
2026-06-30 23:22:46 +0200

    Move random utils into the header only
    The functions will be inlined, so they have to be in the header

--- include/RandomUtils.hpp
+++ include/RandomUtils.hpp
@@ -1,4 +1,17 @@
 #pragma once
 
-double get_zero_to_one();
-double get_random_double(double min, double max);
+#include <random>
+
+inline double get_zero_to_one()
+{
+    thread_local std::random_device rd;
+    thread_local std::mt19937 gen(rd());
+    thread_local std::uniform_real_distribution<double> dist(0.0, 1.0);
+
+    return dist(gen);
+}
+
+inline double get_random_double(double min, double max)
+{
+    return min + (max - min) * get_zero_to_one();
+}
