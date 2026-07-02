9ba9a70446c0a4f3c309f3de0bdfe99c42220f7b
Pandicon <70060103+Pandicon@users.noreply.github.com>
2026-07-02 18:11:30 +0200

    Use pcg rng
    Increases performance with hopefully negligible effects

--- include/RandomUtils.hpp
+++ include/RandomUtils.hpp
@@ -1,14 +1,45 @@
 #pragma once
 
+#include <algorithm>
+#include <cstdint>
 #include <random>
+
+const uint64_t N = 6364136223846793005ULL;
+const double one_over_max = 0x1.0p-32;
+
+inline uint64_t
+get_seed()
+{
+    std::random_device rd;
+    uint64_t seed = (static_cast<uint64_t>(rd()) << 32) | rd();
+    return seed;
+}
+inline uint64_t get_initial_state(uint64_t pcg_seed)
+{
+    uint64_t inc = (pcg_seed << 1) | 1;
+    return (inc + pcg_seed) * N + inc;
+}
+
+inline thread_local uint64_t pcg_seed = get_seed();
+inline thread_local uint64_t inc = (pcg_seed << 1) | 1;
+inline thread_local uint64_t pcg_state = get_initial_state(pcg_seed);
+
+// https://rosettacode.org/wiki/Pseudo-random_numbers/PCG32
+inline uint32_t pcg32_random()
+{
+    uint64_t old_state = pcg_state;
+
+    pcg_state = old_state * N + inc;
+
+    uint32_t xs = static_cast<uint32_t>(((old_state >> 18u) ^ old_state) >> 27u);
+    uint32_t rot = static_cast<uint32_t>(old_state >> 59u);
+
+    return (xs >> rot) | (xs << ((-rot) & 31));
+}
 
 inline double get_zero_to_one()
 {
-    thread_local std::random_device rd;
-    thread_local std::mt19937 gen(rd());
-    thread_local std::uniform_real_distribution<double> dist(0.0, 1.0);
-
-    return dist(gen);
+    return ((double)pcg32_random()) * one_over_max;
 }
 
 inline double get_random_double(double min, double max)
