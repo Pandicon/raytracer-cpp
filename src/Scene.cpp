953482a7b0051799cb9311af2f5522cd55d37fd0
Pandicon <70060103+Pandicon@users.noreply.github.com>
2026-07-02 13:58:55 +0200

    Make the multithreading finer
    Some rows will be more expensive to compute than others, so this splits the work more fairly by assigning smaller chunks to threads. Minor performance increase.

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <atomic>
 #include <thread>
 
 #include "Colour.hpp"
@@ -7,7 +8,9 @@
 const double MINIMUM_T = 0.0001;
 const int MAX_BOUNCES = 10;
 
-void run_render_thread(std::vector<Colour> &pixels, uint32_t start_row, uint32_t end_row, uint32_t width, uint32_t height, const std::vector<std::unique_ptr<Hittable>> &objects_);
+const uint32_t CHUNK_SIZE = 4;
+
+void run_render_thread(std::vector<Colour> &pixels, std::atomic<uint32_t> &next_row, uint32_t width, uint32_t height, const std::vector<std::unique_ptr<Hittable>> &objects_);
 
 Scene::Scene(std::vector<std::unique_ptr<Hittable>> objects) : objects_(std::move(objects)) {}
 
@@ -18,64 +21,71 @@ void Scene::add_object(std::unique_ptr<Hittable> object)
 
 void Scene::render(std::vector<Colour> &pixels, uint32_t width, uint32_t height, uint32_t n_threads)
 {
-    uint32_t rows_per_thread = (height + n_threads - 1) / n_threads;
+    std::atomic<uint32_t> next_row{0};
     std::vector<std::jthread> threads;
     for (int thread_id = 0; thread_id < n_threads; thread_id += 1)
     {
-        uint32_t start_row = rows_per_thread * thread_id;
-        uint32_t end_row = std::min(height, start_row + rows_per_thread);
-        threads.emplace_back(run_render_thread, std::ref(pixels), start_row, end_row, width, height, std::cref(objects_));
+        threads.emplace_back(run_render_thread, std::ref(pixels), std::ref(next_row), width, height, std::cref(objects_));
     }
 }
 
-void run_render_thread(std::vector<Colour> &pixels, uint32_t start_row, uint32_t end_row, uint32_t width, uint32_t height, const std::vector<std::unique_ptr<Hittable>> &objects_)
+void run_render_thread(std::vector<Colour> &pixels, std::atomic<uint32_t> &next_row, uint32_t width, uint32_t height, const std::vector<std::unique_ptr<Hittable>> &objects_)
 {
     double w_d = (double)width;
     double h_d = (double)height;
-    for (uint32_t y = start_row; y < end_row; y += 1)
+    while (true)
     {
-        for (uint32_t x = 0; x < width; x += 1)
+        uint32_t start_y = next_row.fetch_add(CHUNK_SIZE);
+        uint32_t end_y = std::min(start_y + CHUNK_SIZE, height);
+        if (start_y >= height)
         {
-            uint32_t pixel_index = y * width + x;
-            Ray ray = Ray(Vec3(0.0, 0.0, 0.0), Vec3((double(x) - w_d / 2.0) / h_d, (height / 2.0 - (double)y) / h_d, 1.0).normalise());
-            std::optional<Ray> ray_opt = ray;
-            int bounces = 0;
-            Colour final_pixel_colour = Colour(0.0, 0.0, 0.0, 1.0); // Needs to be gathered by hitting lights
-            Colour ray_colour = Colour::white();                    // What is currently being carried around, gets lost at bounces
-            while (ray_opt && bounces <= MAX_BOUNCES)
+            break;
+        }
+        for (uint32_t y = start_y; y < end_y; y += 1)
+        {
+            for (uint32_t x = 0; x < width; x += 1)
             {
-                Ray ray = *ray_opt;
-                double closest_t = std::numeric_limits<double>::max();
-                std::optional<HitRecord> closest_hit = {};
-                for (const auto &object : objects_)
+                uint32_t pixel_index = y * width + x;
+                Ray ray = Ray(Vec3(0.0, 0.0, 0.0), Vec3((double(x) - w_d / 2.0) / h_d, (height / 2.0 - (double)y) / h_d, 1.0).normalise());
+                std::optional<Ray> ray_opt = ray;
+                int bounces = 0;
+                Colour final_pixel_colour = Colour(0.0, 0.0, 0.0, 1.0); // Needs to be gathered by hitting lights
+                Colour ray_colour = Colour::white();                    // What is currently being carried around, gets lost at bounces
+                while (ray_opt && bounces <= MAX_BOUNCES)
                 {
-                    if (auto result = object->hit(ray))
+                    Ray ray = *ray_opt;
+                    double closest_t = std::numeric_limits<double>::max();
+                    std::optional<HitRecord> closest_hit = {};
+                    for (const auto &object : objects_)
                     {
-                        if (result->t > MINIMUM_T && result->t < closest_t)
+                        if (auto result = object->hit(ray))
                         {
-                            closest_t = result->t;
-                            closest_hit = result;
+                            if (result->t > MINIMUM_T && result->t < closest_t)
+                            {
+                                closest_t = result->t;
+                                closest_hit = result;
+                            }
                         }
                     }
-                }
 
-                if (closest_hit)
-                {
-                    Colour emitted = closest_hit->hit_object.get().emitted(ray);
-                    final_pixel_colour = final_pixel_colour + ray_colour * emitted;
+                    if (closest_hit)
+                    {
+                        Colour emitted = closest_hit->hit_object.get().emitted(ray);
+                        final_pixel_colour = final_pixel_colour + ray_colour * emitted;
 
-                    Colour colour_albedo = closest_hit->hit_object.get().colour_contribution(ray);
-                    ray_colour = ray_colour * colour_albedo;
+                        Colour colour_albedo = closest_hit->hit_object.get().colour_contribution(ray);
+                        ray_colour = ray_colour * colour_albedo;
 
-                    ray_opt = closest_hit->hit_object.get().scatter(ray, closest_hit->point, closest_hit->normal);
-                    bounces += 1;
+                        ray_opt = closest_hit->hit_object.get().scatter(ray, closest_hit->point, closest_hit->normal);
+                        bounces += 1;
+                    }
+                    else
+                    {
+                        ray_opt = std::nullopt;
+                    }
                 }
-                else
-                {
-                    ray_opt = std::nullopt;
-                }
+                pixels[pixel_index] = final_pixel_colour;
             }
-            pixels[pixel_index] = final_pixel_colour;
         }
     }
 }
